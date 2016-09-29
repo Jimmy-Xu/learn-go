d6d5e43d48589b534daac4ae4f6a540c3474db65
Jimmy Xu <xjimmyshcn@gmail.com>
2016-09-29 13:27:30 +0800

    [master] finish ping-pong in pratical

--- websocket/gorilla/practical/ping-pong/main.go
+++ websocket/gorilla/practical/ping-pong/main.go
@@ -29,6 +29,30 @@ func main() {
             fmt.Println(err)
             return
         }
+
+        //now, whenever we get a "ping" message, we wait 2 seconds and answer with a "pong" message.
+        //If we get anything else, we just close the connection.
+        for {
+          msgType, msg, err := conn.ReadMessage()
+          if err != nil {
+            fmt.Println(err)
+            return
+          }
+          if string(msg) == "ping" {
+            fmt.Println("ping")
+            time.Sleep(2 * time.Second)
+            err = conn.WriteMessage(msgType, []byte("pong"))
+            if err != nil {
+              fmt.Println(err)
+              return
+            }
+          } else {
+            conn.Close()
+            fmt.Println(string(msg))
+            return
+          }
+        }
+
     })
     http.HandleFunc("/", func(w http.ResponseWriter, r * http.Request) {
         fmt.Fprintf(w, string(index))
