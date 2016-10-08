0efb24811ac85230cf0f0506b8194c82daea9a2a
Jimmy Xu <xjimmyshcn@gmail.com>
2016-10-08 14:03:22 +0800

    [master] add hyper-client.go

--- websocket/gorilla/practice/dialTLS/README.md
+++ websocket/gorilla/practice/dialTLS/README.md
@@ -56,3 +56,39 @@ Open Web Browser:
 
 http://stackoverflow.com/questions/29324251/gorilla-websocket-with-cookie-authentication
 https://github.com/gorilla/websocket/blob/master/client_server_test.go#L322
+
+===================================================
+
+# hyper-client.go
+
+>Connect to websocket api `/events/ws` of Hyper.sh apirouter
+
+- `InsecureSkipVerify: true`
+- Use util/sign4.go
+
+## Run result
+```
+//client
+$ go run hyper-client.go
+connecting to wss://147.75.195.37:6443/events/ws
+URL:wss://147.75.195.37:6443/events/ws
+recv: 2016-10-08 14:16:27.183389957 +0800 CST
+recv: 2016-10-08 14:16:28.183427987 +0800 CST
+recv: 2016-10-08 14:16:29.183434709 +0800 CST
+
+//apirouter: https://github.com/getdvm/hyper-api-router/pull/298
+I1008 06:16:25.812586   28249 key.go:31] check V4 signature
+I1008 06:16:25.813984   28249 tbac.go:122] Authorize tenant 1ca724a038554e89a2647100b004d2fc (restricted: false) via key jimmy+test@hyper.sh for action , status 0
+I1008 06:16:25.813996   28249 tbac.go:72] Authorize for resource  from user jimmy+test@hyper.sh, result : <nil>
+I1008 06:16:25.814085   28249 log.go:8] APIRequest[1475907385814082037]: Start, Tenant: 1ca724a038554e89a2647100b004d2fc, Verb: GET, Path: /events/ws
+```
+
+## FAQ:
+### wrong Hyper.sh Credential
+```
+$ go run hyper-client.go
+connecting to wss://147.75.195.37:6443/events/ws
+URL:wss://147.75.195.37:6443/events/ws
+dial:websocket: bad handshake
+exit status 1
+```
---
+++ websocket/gorilla/practice/dialTLS/hyper-client.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	SignUtil "github.com/Jimmy-Xu/learn-go/websocket/gorilla/practice/dialTLS/util"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var addr = flag.String("addr", "147.75.195.37:6443", "http service address")
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "wss", Host: *addr, Path: "/events/ws"}
+	log.Printf("connecting to %s", u.String())
+
+	config := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	dialer := websocket.Dialer{
+		TLSClientConfig: config,
+	}
+
+	fmt.Printf("URL:%v\n", u.String())
+	req, err := http.NewRequest("GET", u.String(), nil)
+	req.URL = &u
+
+	accessKey := "6DVNAWRWDP6NUVGEOLKGJ9YV"
+	secretKey := "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	req = SignUtil.Sign4(accessKey, secretKey, req)
+
+	c, _, err := dialer.Dial(u.String(), req.Header)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+
+	go func() {
+		defer c.Close()
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			log.Printf("recv: %s", message)
+		}
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case t := <-ticker.C:
+			err := c.WriteMessage(websocket.TextMessage, []byte(t.String()))
+			if err != nil {
+				log.Println("write:", err)
+				return
+			}
+		case <-interrupt:
+			log.Println("interrupt")
+			// To cleanly close a connection, a client should send a close
+			// frame and wait for the server to close the connection.
+			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+			if err != nil {
+				log.Println("write close:", err)
+				return
+			}
+			select {
+			case <-done:
+			case <-time.After(time.Second):
+			}
+			c.Close()
+			return
+		}
+	}
+}
