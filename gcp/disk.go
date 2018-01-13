4b9d38e49f180bdc703c8b197c8fd2b4cbc3643c
Jimmy Xu <xjimmyshcn@gmail.com>
2018-01-13 22:22:59 +0800

    [master] support get,list,delete disk

--- gcp/disk.go
+++ gcp/disk.go
@@ -25,18 +25,17 @@ REF:
 import (
 	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
-	"google.golang.org/api/compute/v0.beta"
+	compute "google.golang.org/api/compute/v0.beta"
 )
 
 var (
 	project = flag.String("project", "hyper-test-142007", "project name")
 	zone    = flag.String("zone", "asia-east1-a", "zone name")
-	disk    = flag.String("disk", "test_disk_api", "disk name")
+	disk    = flag.String("disk", "test-disk", "disk name")
 	filter  = flag.String("filter", "", "filter expression for filtering listed resources")
 	create  = flag.Bool("create", false, "create new disk or not")
 )
@@ -57,26 +56,83 @@ func main() {
 		log.Fatal(err)
 	}
 
+	err = createDisk(ctx, computeService)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	err = listDisk(ctx, computeService)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	err = getDisk(ctx, computeService)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = deleteDisk(ctx, computeService)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func createDisk(ctx context.Context, computeService *compute.Service) error {
+	rb := &compute.Disk{
+		SizeGb:      1,
+		Name:        *disk,
+		Description: "test",
+	}
+	resp, err := computeService.Disks.Insert(*project, *zone, rb).Context(ctx).Do()
+	if err != nil {
+		return err
+	}
+	printJson("create disk", resp)
+	return nil
 }
 
 func listDisk(ctx context.Context, computeService *compute.Service) error {
 	req := computeService.Disks.List(*project, *zone)
 	if err := req.Filter(*filter).Pages(ctx, func(page *compute.DiskList) error {
-		fmt.Printf("there are %v disk in zone:%v\n", len(page.Items), *zone)
+		log.Printf("there are %v disk in zone:%v\n", len(page.Items), *zone)
+		log.Printf("list disk:\npage.Items: %T", page.Items)
 		for _, disk := range page.Items {
 			diskJson, err := json.MarshalIndent(disk, "", "\t")
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Printf("%v\n", string(diskJson))
+			log.Printf("disk: %T - %v\n", disk, string(diskJson))
 		}
 		return nil
 	}); err != nil {
 		return err
 	}
 	return nil
+}
+
+func getDisk(ctx context.Context, computeService *compute.Service) error {
+	resp, err := computeService.Disks.Get(*project, *zone, *disk).Context(ctx).Do()
+	if err != nil {
+		return err
+	}
+	printJson("get disk", resp)
+	return nil
+}
+
+func deleteDisk(ctx context.Context, computeService *compute.Service) error {
+	resp, err := computeService.Disks.Delete(*project, *zone, *disk).Context(ctx).Do()
+	if err != nil {
+		return err
+	}
+	printJson("delete disk", resp)
+	return nil
+}
+
+func printJson(action string, data interface{}) {
+	buf, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%s: %T - %v\n", action, data, string(buf))
 }
