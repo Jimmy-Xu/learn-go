2f4052dedaab6dff3a99272727bc79692d511731
Jimmy Xu <xjimmyshcn@gmail.com>
2018-01-16 17:09:24 +0800

    [master] list k8s pod via client-go

---
+++ kubernetes/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"path/filepath"
+
+	apiv1 "k8s.io/api/core/v1"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func main() {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+	opts := meta_v1.ListOptions{}
+	podList, err := clientset.CoreV1().Pods(apiv1.NamespaceDefault).List(opts)
+
+	buf, err := json.MarshalIndent(*podList, "", " ")
+	fmt.Printf("podList:\n%v\n", string(buf))
+}
